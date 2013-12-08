e242d4596dd5491c4e3a9de733eeb3795494be8a
nettee <xf0718@gmail.com>
2013-12-08 20:05:26 +0800

    update

--- pandim.py
+++ pandim.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+# version: 0.7.4
+# 2013-12-8
+
 """ pandim: html file generator
 use yate as support
 """
@@ -10,19 +13,56 @@ import argparse
 
 import yate
     
-def pandim(line, args):
-    line = line.rstrip('\n')
-    if line.startswith('#'):
-        npounds = re.match(r'#+', line).group(0)
-        line = line.lstrip('#').lstrip(' ')
-        line = yate.header(line, len(npounds))
+def pandim(args):
+    if args.filename:
+        fobj = args.filename
     else:
-        line = yate.para(line)
+        fobj = sys.stdin
 
-    return line
+    buf = []
+    IN_CODE = False
+    IN_QUOTE = False
+
+    for line in fobj:
+        line = line.rstrip('\n')
+        if line.startswith('\\'):
+            option = re.search(r'\\(\w+)', line).group(1)
+            if option == 'code':
+                IN_CODE = True
+                buf = []
+                continue
+            elif option == 'endcode':
+                line = yate.code(buf)
+                IN_CODE = False
+            elif option == 'quote':
+                IN_QUOTE = True
+                buf = []
+                continue
+            elif option == 'endquote':
+                line = yate.blockquote(buf)
+                IN_QUOTE = False
+        elif IN_CODE:
+            buf.append(line)
+            continue
+        elif IN_QUOTE:
+            buf.append(yate.para(line))
+            continue
+        elif line.startswith('#'):
+            npounds = re.match(r'#+', line).group(0)
+            line = line.lstrip('#').lstrip(' ')
+            line = yate.header(line, len(npounds))
+        elif line.startswith('>'):
+            line = line.lstrip('>').lstrip(' ')
+            line = yate.blockquote(yate.para(line))
+        elif not line:
+            continue
+        else:
+            line = yate.para(line)
+        print(line)
 
 def parse_args():
     parser = argparse.ArgumentParser()
+    parser.add_argument('filename', nargs='?')
     parser.add_argument('-t', '--title')
     
     return parser.parse_args()
@@ -43,7 +83,6 @@ if __name__ == '__main__':
     else:
         print(templ_1.format(title='a.html'))
 
-    for line in sys.stdin:
-        line = pandim(line, args)
-        print(line)
+    pandim(args)
+
     print(templ_2)
--- yate.py
+++ yate.py
@@ -1,15 +1,41 @@
 #!/usr/bin/env python3
 
+# version: 0.2.1
+# 2013-12-8
+
 """ yate: html generator
 """
 
+import re
+
+def escape(line):
+    esc_map = {'&' : '&amp;',
+            '<' : '&lt;',
+            '>' : '&gt;',
+        }
+    return ''.join(esc_map[c] if c in esc_map else c for c in line)
+
 def para(p):
+    p = escape(p)
+    def _code_inline(mobj):
+        return code_inline(mobj.group(1))
+    p = re.sub(r'``(.*)``', _code_inline, p)
     return '<p>{}</p>'.format(p)
 
 def header(s, level=3):
-    return '<h{0}>{1}</h{0}>'.format(level, s)
+    return '<h{0}>{1}</h{0}>'.format(level, escape(s))
 
 def comment(s):
-    return '<!-- {} -->'.format(s)
+    return '<!-- {} -->'.format(escape(s))
+
+def blockquote(lines):
+    return ("<blockquote>" + '\n'.join(lines) + "</blockquote>")
+
+def code(lines):
+    return "<pre><code>" + '\n'.join(lines) + "</code></pre>"
+
+def code_inline(s):
+    return "<code>{}</code>".format(s)
+
 
 
